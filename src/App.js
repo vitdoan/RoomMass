68cb600f01616a79f348ccf3a4fb6e59b5ba7206
Long Huynh <longbhuynh019@gmail.com>
2022-11-12 17:19:48 -0500

    added 2 buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Navigation/Navigation'
+import Home from './components/Home/Home'
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Home/>
       
     </div>
   );
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -23,7 +23,7 @@ class Header extends Component {
 
   toggleModal() {
     this.setState({
-      isModalOpen: !this.state.isModalOpen
+      isModalOpen: !this.state.isModalOpen,
     });
   }
 
@@ -48,125 +48,132 @@ class Header extends Component {
               Home
             </a>
           </li>
+
           <li className="nav-item">
             <a className="nav-link" href="#">
               About
             </a>
           </li>
-          <li className="nav-item">
-          <div>
-          <button
-            type="button"
-            className="btn btn-success"
-            onClick={this.toggleModal}
-          >
-            Log In
-          </button>
 
-          <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
-            <ModalHeader toggle={this.toggleModal}>Log In</ModalHeader>
-            <ModalBody>
-              <Form onSubmit={this.handleLogIn}>
-                <FormGroup>
-                  <Label htmlFor="username">Username</Label>
-                  <Input
-                    type="text"
-                    id="username"
-                    name="username"
-                    innerRef={(input) => (this.name = input)}
-                  />
-                </FormGroup>
-                <FormGroup>
-                  <Label htmlFor="password">Password</Label>
-                  <Input
-                    type="text"
-                    id="password"
-                    name="password"
-                    innerRef={(input) => (this.type = input)}
-                  />
-                </FormGroup>
-                <FormGroup check>
-                  <Label check>
-                    <Input
-                      type="checkbox"
-                      name="remember"
-                      innerRef={(input) => (this.remember = input)}
-                    />
-                    Remember me
-                  </Label>
-                </FormGroup>
-                <Button className="mt-3" type="submit" value="submit" color="primary">
-                  Log In
-                </Button>
-              </Form>
-            </ModalBody>
-          </Modal>
-        </div>
-            
-           
+          <li className="nav-item">
+            <div>
+              <button
+                type="button"
+                className="btn btn-success"
+                onClick={this.toggleModal}
+              >
+                Log In
+              </button>
+
+              <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                <ModalHeader toggle={this.toggleModal}>Log In</ModalHeader>
+                <ModalBody>
+                  <Form onSubmit={this.handleLogIn}>
+                    <FormGroup>
+                      <Label htmlFor="username">Username</Label>
+                      <Input
+                        type="text"
+                        id="username"
+                        name="username"
+                        innerRef={(input) => (this.name = input)}
+                      />
+                    </FormGroup>
+                    <FormGroup>
+                      <Label htmlFor="password">Password</Label>
+                      <Input
+                        type="text"
+                        id="password"
+                        name="password"
+                        innerRef={(input) => (this.type = input)}
+                      />
+                    </FormGroup>
+                    <FormGroup check>
+                      <Label check>
+                        <Input
+                          type="checkbox"
+                          name="remember"
+                          innerRef={(input) => (this.remember = input)}
+                        />
+                        Remember me
+                      </Label>
+                    </FormGroup>
+                    <Button
+                      className="mt-3"
+                      type="submit"
+                      value="submit"
+                      color="primary"
+                    >
+                      Log In
+                    </Button>
+                  </Form>
+                </ModalBody>
+              </Modal>
+            </div>
           </li>
-          <li className="nav-item">
-          <button
-            type="button"
-            className="btn"
-            onClick={this.toggleModal}
-          >
-            Sign Up
-          </button>
 
-          <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
-            <ModalHeader toggle={this.toggleModal}>Sign up</ModalHeader>
-            <ModalBody>
-              <Form onSubmit={this.handleLogIn}>
-                <FormGroup>
-                  <Label htmlFor="username">Username</Label>
-                  <Input
-                    type="text"
-                    id="username"
-                    name="username"
-                    innerRef={(input) => (this.name = input)}
-                  />
-                </FormGroup>
-                <FormGroup>
-                  <Label htmlFor="password">Password</Label>
-                  <Input
-                    type="text"
-                    id="password"
-                    name="password"
-                    innerRef={(input) => (this.type = input)}
-                  />
-                </FormGroup>
-                <FormGroup check>
-                  <Label check>
+          <li className="nav-item">
+            <button type="button" className="btn" onClick={this.toggleModal}>
+              Sign Up
+            </button>
+
+            <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+              <ModalHeader toggle={this.toggleModal}>Log In</ModalHeader>
+              <ModalBody>
+                <Form onSubmit={this.handleLogIn}>
+                  <FormGroup>
+                    <Label htmlFor="username">Username</Label>
                     <Input
-                      type="checkbox"
-                      name="remember"
-                      innerRef={(input) => (this.remember = input)}
+                      type="text"
+                      id="username"
+                      name="username"
+                      innerRef={(input) => (this.name = input)}
                     />
-                    Remember me
-                  </Label>
-                </FormGroup>
-                <Button className="mt-3" type="submit" value="submit" color="primary">
-                  Log In
-                </Button>
-              </Form>
-            </ModalBody>
-          </Modal>
+                  </FormGroup>
+                  <FormGroup>
+                    <Label htmlFor="password">Password</Label>
+                    <Input
+                      type="text"
+                      id="password"
+                      name="password"
+                      innerRef={(input) => (this.type = input)}
+                    />
+                  </FormGroup>
+                  <FormGroup check>
+                    <Label check>
+                      <Input
+                        type="checkbox"
+                        name="remember"
+                        innerRef={(input) => (this.remember = input)}
+                      />
+                      Remember me
+                    </Label>
+                  </FormGroup>
+                  <Button
+                    className="mt-3"
+                    type="submit"
+                    value="submit"
+                    color="primary"
+                  >
+                    Log In
+                  </Button>
+                </Form>
+              </ModalBody>
+            </Modal>
           </li>
         </ul>
 
-        <div className = " p-5  bg-secondary text-white rounded">
-            <div className="container">
-                <div className="row row-header">
-                    <div className="col-12 col-sm-6">
-                        <h1>Swap Umass</h1>
-                        <p>Having troubles finding people to swap your room? We got you!</p>
-                    </div>
-                </div>
+        <div className="mb-5 p-5  bg-secondary text-white rounded">
+          <div className="container">
+            <div className="row row-header">
+              <div className="col-12 col-sm-6">
+                <h1>Swap Umass</h1>
+                <p>
+                  Having troubles finding people to swap your room? We got you!
+                </p>
+              </div>
             </div>
+          </div>
         </div>
-
-       
       </>
     );
   }
