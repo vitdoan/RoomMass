94b2a1fb2d19f8aac45914959479e995228e1276
Vi Doan <viidoan2505@gmail.com>
2022-11-13 03:13:39 -0500

    resolve conflict

--- src/components/Swap/SwapPost.js
+++ src/components/Swap/SwapPost.js
@@ -5,7 +5,7 @@ export default function SwapPost({post}) {
         <div className="card-body cardPostDetail p-3">
             <h5 className="card-title">Swap From: {post.area}</h5>
             <h6 className="card-subtitle mb-2 text-muted">Posted by: {post.username}</h6>
-            <p className="card-text">Gender: {post.hobby}</p>
+            <p className="card-text">Gender: {post.gender}</p>
             <p className="card-text">Hobbies: {post.hobby}</p>
             <p className="card-text">Reason: {post.reason}</p>
             <p className="card-text">Looking for: {post.extra_note}</p>
