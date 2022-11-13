5ddb65494177129bbb5b1730e7e420b01d0d6789
Long Huynh <longbhuynh019@gmail.com>
2022-11-13 09:14:56 -0500

    added contact us route

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Dashboard.css";
-import {SiHomeadvisor} from 'react-icons/si'
+import {HiHome} from 'react-icons/hi'
 
 export default function Dashboard({isSignedIn, changeRoute}) {
 	return (
@@ -8,7 +8,7 @@ export default function Dashboard({isSignedIn, changeRoute}) {
         <nav class="navbar navbar-light bg-light justify-content-between dashboard">
             <a class="navbar-brand " style = {{cursor:"default"}}>Room<span style={{ color: "#E6AC2C", cursor: "default" }}>Mass</span></a>
             <div> 
-            <button class="btn btn-outline-warning my-2 my-sm-0" onClick={()=>changeRoute(3)}><SiHomeadvisor/></button>
+            <button class="btn btn-outline-warning my-2 my-sm-0" onClick={()=>changeRoute(3)}><HiHome/></button>
             <button class="btn btn-outline-warning my-2 my-sm-0" onClick={()=>changeRoute(5)}>Swap Room</button>
             <button class="btn btn-outline-warning my-2 my-sm-0" onClick={()=>changeRoute(4)}>Find Roommate</button>
             {isSignedIn?<button class="btn btn-outline-warning my-2 my-sm-0" onClick={()=>changeRoute(1)}>Signout</button>:
