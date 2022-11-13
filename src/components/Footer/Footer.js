2105ca187205ae6445843dec6b2831368cb67981
Long Huynh <longbhuynh019@gmail.com>
2022-11-13 15:32:34 -0500

    updated footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -22,7 +22,7 @@ const Footer = () => {
             </div>
 
             <div className="footer__copyright">
-                <small>&copy; 2022 by Vi, Long, Phuoc, Quang. All rights reserved.</small>
+                <small>&copy; 2022 by Vi, Long, and Phuoc. All rights reserved.</small>
             </div>
         </footer>
 
