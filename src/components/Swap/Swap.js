75dc3d20f4da743ab7ca47e29720294918134a1d
Long Huynh <longbhuynh019@gmail.com>
2022-11-12 21:27:42 -0500

    updated swap component

--- src/components/Swap/Swap.js
+++ src/components/Swap/Swap.js
@@ -91,39 +91,109 @@ export default function Swap() {
       </div>
 
       <Modal isOpen={isModalOpen} toggle={toggleModal}>
-        <ModalHeader toggle={toggleModal}>Log In</ModalHeader>
+        <ModalHeader toggle={toggleModal}>Add A Post</ModalHeader>
         <ModalBody>
           <Form>
             <FormGroup>
-              <Label htmlFor="username">Username</Label>
-              <Input
-                type="text"
-                id="username"
-                name="username"
-                // innerRef={(input) => (this.username = input)}
-              />
+              <Label htmlFor="username">Gender</Label>
+              <select class="form-select" aria-label="Default select example">
+                <option selected>Select</option>
+                <option value="1">Female</option>
+                <option value="2">Male</option>
+                <option value="3">Bede</option>
+              </select>
+            </FormGroup>
+
+            <FormGroup>
+              <Label htmlFor="password">School Year</Label>
+              <select class="form-select" aria-label="Default select example">
+                <option selected>Select</option>
+                <option value="Freshman">Freshman</option>
+                <option value="Sophomore">Sophomore</option>
+                <option value="Junior">Junior</option>
+                <option value="Senior">Senior</option>
+              </select>
+            </FormGroup>
+
+            <FormGroup>
+              <Label htmlFor="password">Working Time: </Label>
+              <div>
+                <div class="form-check form-check-inline ">
+                  <input
+                    class="form-check-input"
+                    type="radio"
+                    name="inlineRadioOptions"
+                    id="inlineRadio1"
+                    value="option1"
+                  ></input>
+                  <label class="form-check-label" for="inlineRadio1">
+                    Late Owl
+                  </label>
+                </div>
+                <div class="form-check form-check-inline ">
+                  <input
+                    class="form-check-input"
+                    type="radio"
+                    name="inlineRadioOptions"
+                    id="inlineRadio2"
+                    value="option2"
+                  ></input>
+                  <label class="form-check-label" for="inlineRadio2">
+                    Early Bird
+                  </label>
+                </div>
+              </div>
+            </FormGroup>
+
+            <FormGroup>
+              <Label htmlFor="password">Area</Label>
+              <select class="form-select" aria-label="Default select example">
+                <option selected>Select</option>
+                <option value="North">North</option>
+                <option value="Northeast">Northeast</option>
+                <option value="Central">Central</option>
+                <option value="Southwest">Southwest</option>
+                <option value="Orchard Hill">Orchard Hill</option>
+                <option value="CHC">CHC</option>
+              </select>
+            </FormGroup>
+
+            <FormGroup>
+              <Label htmlFor="password">Hall</Label>
+              <select class="form-select" aria-label="Default select example">
+                <option selected>Select</option>
+                <option value="Freshman">Sylvan</option>
+                <option value="Sophomore">Leach</option>
+                <option value="Junior">Junior</option>
+                <option value="Senior">Senior</option>
+              </select>
             </FormGroup>
             <FormGroup>
-              <Label htmlFor="password">Password</Label>
-              <Input
-                type="password"
-                id="password"
-                name="password"
-                //innerRef={(input) => (this.password = input)}
-              />
+              <Label htmlFor="password">Why do you want to swap?</Label>
+              <div class="mb-3">
+                
+                <textarea
+                  class="form-control"
+                  id="exampleFormControlTextarea1"
+                  rows="3"
+                ></textarea>
+              </div>
             </FormGroup>
-            <FormGroup check>
-              <Label check>
-                <Input
-                  type="checkbox"
-                  name="remember"
-                  //innerRef={(input) => (this.remember = input)}
-                />
-                Remember me
-              </Label>
+            <FormGroup>
+              <Label htmlFor="password">Looking For?</Label>
+              <div class="mb-3">
+                
+                <textarea
+                  class="form-control"
+                  id="exampleFormControlTextarea1"
+                  rows="3"
+                  placeholder = "Hall Preferences: Cashin, Leach, etc. "
+                ></textarea>
+              </div>
             </FormGroup>
-            <Button type="submit" value="submit" color="primary">
-              Login
+
+            <Button type="submit" value="submit" color="success">
+             Add
             </Button>
           </Form>
         </ModalBody>
