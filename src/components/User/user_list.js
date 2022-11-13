03f6d905e54f21d89b24544162b747a4273ed45a
Patrick Do <phuocdo@umass.edu>
2022-11-13 04:32:12 -0500

    user list for login

---
+++ src/components/User/user_list.js
@@ -0,0 +1,49 @@
+let user1 = {
+    id: 101,
+    name: "Ali",
+    username: "AliHD",
+    password: "ali123",
+    joined: new Date()
+}
+
+let user2 = {
+    id: 102,
+    name: "Adam",
+    username: "AdamL230",
+    password: "adamadam",
+    joined: new Date()
+}
+
+let user3 = {
+    id: 103,
+    name: "JayT",
+    username: "JayT37",
+    password: "justatee37",
+    joined: new Date()
+}
+
+let user4 = {
+    id: 104,
+    name: "Lynklee",
+    username: "Lynkleelee",
+    password: "tambietnhe",
+    joined: new Date()
+}
+
+let user5 = {
+    id: 105,
+    name: "Camly",
+    username: "EmMinhTuyet",
+    password: "khongbietbaigi",
+    joined: new Date()
+}
+
+let user6 = {
+    id: 106,
+    name: "Bruce Lee",
+    username: "BruceLee234",
+    password: "ChineseMartialArt",
+    joined: new Date()
+}
+
+user_list = [user1, user2, user3, user4, user5, user6];
